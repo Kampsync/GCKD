8b5cdade4dbfce86f7dc1f8af56c9b9a7227d4cc
Kampsync <bookercampers@gmail.com>
2025-06-24 01:19:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ app.get('/v1/ical/:icalKey', async (req, res) => {
 
     const output = ics.join('\r\n');
 
-    // Fix headers to stop forced downloads
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    // FINAL FIX: HEADERS THAT OUTDOORSY ACCEPTS
+    res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
     res.setHeader('Content-Disposition', 'inline');
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.status(200).send(output);
